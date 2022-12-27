2495c4dc1c8976cbfab7c4507829fbc5abfe14eb
gongpoman <wjdgmlwh1629343@gmail.com>
2022-12-27 20:53:27 +0900

    neighbor search with z indexing

--- PCISPH/main.cpp
+++ PCISPH/main.cpp
@@ -17,14 +17,15 @@
 
 
 void processInput(GLFWwindow* window);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void __mapBuffer(unsigned int& vbo, void* data, unsigned int size);
 
 void updateParticles();
 void particleInit(int mode);
+void gridInit(std::vector<unsigned int>*);
 void instanceMat(glm::mat4*);
+void neighborSearch(std::vector<Particle>* neighbors,unsigned int Idx);
 
 
 void __cubeBoundaryCellIdx();
@@ -33,16 +34,14 @@ unsigned int __cubeMorton(int x, int y, int z);
 
 extern const unsigned int SCR_WIDTH = 1280;
 extern const unsigned int SCR_HEIGHT = 720;
-Camera cam(glm::vec3(3.0f, 7.0f, 7.0f));
+Camera cam(glm::vec3(0.0f, 7.0f, 7.0f));
 
 float lastX, lastY;
 bool isFirstMove = true;
 
 
-
-
 extern const float deltaTime = 1/60.0f;
-const unsigned int numParticle = 40*40*40;
+const unsigned int numParticle = 50*50*50 ;
 const glm::vec3 acc_g = glm::vec3(0.0f,-9.8f,0.0f);
 
 const float restDensity = 997.0f;
@@ -50,14 +49,19 @@ const float radius = 0.03f;
 const float particleMass = restDensity*4.0f / 3.0f * 3.141592f * radius * radius * radius;
 const float coreRad = radius * 6.0f; //  (= grid side length, h )
 
+
 Particle* particles = NULL;
-vector<unsigned int> mortonIdx;
 vector<unsigned int> mortonCount;
+std::map<unsigned int, unsigned int> idxMap;
+
+vector<unsigned int>* neighborIdices;
+
 
 // 정육면체 grid 가정함.
 const float cubicBoundaryLen = 5.0f;
 unsigned int nGridDivisoin = (unsigned int)(cubicBoundaryLen / coreRad);
 
+void outBoundarySolution(Particle*);
 
 
 int main()
@@ -85,9 +89,8 @@ int main()
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
     glEnable(GL_DEPTH_TEST);
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
+//    glfwSetCursorPosCallback(window, mouse_callback);
+//    glfwSetScrollCallback(window, scroll_callback);
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 
@@ -134,6 +137,11 @@ int main()
 
         glBindVertexArray(0);
     }
+
+    //grid Init
+    neighborIdices = new std::vector<unsigned int>[nGridDivisoin * nGridDivisoin * nGridDivisoin];
+    gridInit(neighborIdices);
+
 
     while (!glfwWindowShouldClose(window))
     {
@@ -185,7 +193,7 @@ void particleInit(int mode) {
                 for (unsigned int k = 0; k < numRow; k++) {
                     
                     particles[i * numRow * numRow + numRow * j + k].pos = glm::vec3(sideLen / 2.0f - sideLen * (i / (float)numRow)
-                        ,sideLen - sideLen * (j / (float)numRow)
+                        ,sideLen - sideLen * (j / (float)numRow) + 3.9f
                     ,sideLen / 2.0f - sideLen * (k / (float)numRow));
                     
                 }
@@ -204,16 +212,31 @@ void instanceMat(glm::mat4* instWorlds) {
 void updateParticles() {
     // 2 TODOexternal force, boundary...
 
-    //1 set grid. particles are sorted. offseted with mortonCounter
+    //1 set grid. particles sorting. offset is saved in mortonCount, mortonIdx
     __cubeBoundaryCellIdx(); 
 
+    std::vector<Particle>* neighbors = new std::vector<Particle>();
 
+
+    unsigned int currentCellIdx = -1;
+    unsigned int prevCellIdx = -1;
     for (unsigned int particleIdx = 0; particleIdx < numParticle; particleIdx++) {
 
+        // neighbor search할건데. particle이 정렬해놨음. 그러니까. 이전 particle과 같은 cellIdx에 속해있는 녀석에 대해서 이웃들을 구할 때 다시 neighbor 찾을 필요 없음.
+        currentCellIdx = particles[particleIdx].cellIdx;
+        if (prevCellIdx != currentCellIdx) {
+            neighbors->clear();
+            neighborSearch(neighbors, particleIdx);
+        }
+
         particles[particleIdx].force_vis = glm::vec3(0);
+
         particles[particleIdx].force_ext = glm::vec3(0);// 2 TODOexternal force, boundary...
+
         particles[particleIdx].pressure = 0.0f;
         particles[particleIdx].force_p = glm::vec3(0);
+
+        prevCellIdx = currentCellIdx;
     }
 
     for (unsigned int particleIdx = 0; particleIdx < numParticle; particleIdx++) {
@@ -221,20 +244,57 @@ void updateParticles() {
         acc /= particleMass;
         particles[particleIdx].vel += (acc + acc_g) * deltaTime;
         particles[particleIdx].pos+= particles[particleIdx].vel* deltaTime;
+        outBoundarySolution(&particles[particleIdx]);
     }
 
 
-
+    delete neighbors;
 }
 
+void outBoundarySolution(Particle* particle) {
+    glm::ivec3 cellIdx = __cubeCellCoord(particle->pos);
+    glm::ivec3 flag;
+
+    flag.x = (cellIdx.x == -1) ? -1 : (cellIdx.x >= nGridDivisoin) ? 1 : 0;
+    flag.z = (cellIdx.z == -1) ? -1 : (cellIdx.z >= nGridDivisoin) ? 1 : 0;
+    flag.y = (cellIdx.y == -1) ? -1 : (cellIdx.y >= nGridDivisoin) ? 1 : 0;
+
+
+    if (flag.x == -1) {
+        particle->vel.x *= -1;
+        particle->pos.x += 2*(-cubicBoundaryLen / 2.0f - particle->pos.x);
+    }
+    if (flag.x == 1) {
+        particle->vel.x *= -1;
+        particle->pos.x += 2 * (cubicBoundaryLen / 2.0f - particle->pos.x);
+    }
+
+    if (flag.z == -1) {
+        particle->vel.z *= -1;
+        particle->pos.z += 2 * (-cubicBoundaryLen / 2.0f - particle->pos.z);
+    }
+    if (flag.z == 1) {
+        particle->vel.z *= -1;
+        particle->pos.z += 2 * (cubicBoundaryLen / 2.0f - particle->pos.z);
+    }
+
+    if (flag.y == -1) {
+        particle->vel.y *= -1;
+        particle->pos.y += 2 * (-1.0f - particle->pos.y);
+    }
+    if (flag.y == 1) {
+        particle->vel.y *= -1;
+        particle->pos.y += 2 * (-1.0f + cubicBoundaryLen - particle->pos.y);
+    }
+
+}
 
 void __cubeBoundaryCellIdx() {          // z indexing.에 맞도록 정렬을 하고, 정렬된 particles array에서 어느 idx부터 어디까지가 어떤 cellIdx를 가지는 particle인지 다 저장해 놓는 코드.
 
     mortonCount.clear(); // 어느 idx부터 d어느 idx까지.
-    mortonIdx.clear(); // 위의 idx 범위가 나타내는게 어느 cellIdx인지.
 
     unsigned int* mortonCounter = new unsigned[nGridDivisoin * nGridDivisoin * nGridDivisoin]; // temp
-    unsigned int outBounardyCount = 0;
+    unsigned int outBoundardyCount = 0;
 
     for (unsigned int i = 0; i < nGridDivisoin * nGridDivisoin * nGridDivisoin; i++)
         mortonCounter[i] = 0;
@@ -246,28 +306,27 @@ void __cubeBoundaryCellIdx() {          // z indexing.에 맞도록 정렬을 하고, 정�
         particles[i].cellIdx = __cubeMorton(cellCoord.x, cellCoord.y, cellCoord.z);
 
         if (particles[i].cellIdx == (unsigned int)( - 1))
-            outBounardyCount++;
+            outBoundardyCount++;
         else
             mortonCounter[particles[i].cellIdx]++;
         
     }
 
+    if (outBoundardyCount > 0) {
+        std::cout << "out boundary particle exist" << std::endl;
+        system("PAUSE");
+    }
+
     unsigned int invIdx = 0;
-    std::map<unsigned int, unsigned int> idxMap;
+    
+    idxMap.clear();
 
     for (unsigned int i = 0; i < nGridDivisoin * nGridDivisoin * nGridDivisoin; i++)
         if (mortonCounter[i] > 0) {
-            mortonIdx.push_back(i);
             mortonCount.push_back(mortonCounter[i]);
-            idxMap.insert(std::pair<unsigned int, unsigned int>(i, invIdx));
+            idxMap.insert(std::pair<unsigned int, unsigned int>(i, invIdx++));
         }
-    if (outBounardyCount > 0) {
-        mortonCount.push_back(outBounardyCount);
-        mortonIdx.push_back((unsigned int)(- 1));
-        idxMap.insert(std::pair<unsigned int, unsigned int>( ((unsigned int)(-1)) , invIdx )) ;
-    }
     delete[] mortonCounter;
-
 
 
     if(mortonCount.size()>0)
@@ -275,7 +334,7 @@ void __cubeBoundaryCellIdx() {          // z indexing.에 맞도록 정렬을 하고, 정�
             *(iter) += *(iter - 1);
 
     std::vector<std::vector<Particle>> tempParticles;
-    for (unsigned int i = 0; i < mortonIdx.size(); i++) 
+    for (unsigned int i = 0; i < mortonCount.size(); i++) 
         tempParticles.push_back(std::vector<Particle>());
 
     for (unsigned int i = 0; i < numParticle; i++) {
@@ -293,15 +352,66 @@ void __cubeBoundaryCellIdx() {          // z indexing.에 맞도록 정렬을 하고, 정�
 }
 
 
+void gridInit(std::vector<unsigned int>* indices) {
+
+    unsigned int idx = 0;
+    for (unsigned int i = 0; i < nGridDivisoin; i++) {
+        for (unsigned int j = 0; j < nGridDivisoin; j++) {
+            for (unsigned int k = 0; k < nGridDivisoin; k++) {
+
+                glm::ivec3 coord;
+
+
+                for (int ii = -1; ii < 2; ii++) {
+                    for (int jj = -1; jj < 2; jj++) {
+                        for (int kk = -1; kk < 2; kk++) {
+
+                            coord = glm::ivec3(i + ii, j + jj, k + kk);
+                            unsigned int cellIdx = __cubeMorton(coord.x, coord.y, coord.z);
+
+                            if (cellIdx != (unsigned int)(-1))
+                                indices[idx].push_back(cellIdx);
+                        }
+                    }
+                }
+
+                idx++;
+            }
+        }
+    }
+    
+}
+
+
+// TODO 이거 한번 어떤 cellIdx에 대해서 구한 neighbor들은 다시 쓸 수 있을거니까. 어떻게든 여기서 시간 조금 더 줄일 수도 있을 것 같음.
+void neighborSearch(std::vector<Particle>* neighbors,unsigned int idx) {
+    for (unsigned int i = 0; i < neighborIdices[particles[idx].cellIdx].size(); i++) {
+        unsigned int cidx = neighborIdices[particles[idx].cellIdx][i];
+        cidx = idxMap[cidx];
+        unsigned int startPoint = mortonCount[cidx];
+        unsigned int endPoint = (cidx>=mortonCount.size()-1)? numParticle :mortonCount[cidx + 1];
+
+        for (unsigned int j = startPoint; j < endPoint; j++) {
+            neighbors->push_back(particles[j]);
+        }
+    }
+}
+
+
 glm::ivec3 __cubeCellCoord(glm::vec3 pos) {
-    unsigned int x = (unsigned int)((pos.x + cubicBoundaryLen / 2.0f) / cubicBoundaryLen * nGridDivisoin);
-    unsigned int y = (unsigned int)(pos.y / cubicBoundaryLen * nGridDivisoin);
-    unsigned int z = (unsigned int)((pos.z + cubicBoundaryLen / 2.0f) / cubicBoundaryLen * nGridDivisoin);
-    return glm::ivec3(x, y, z);
+
+    float x = ((pos.x + cubicBoundaryLen / 2.0f) / cubicBoundaryLen * nGridDivisoin);
+    float y = ((pos.y + 1.0f) / cubicBoundaryLen * nGridDivisoin);
+    float z = ((pos.z + cubicBoundaryLen / 2.0f) / cubicBoundaryLen * nGridDivisoin);
+
+    x = (x + FLT_EPSILON <= 0) ? -1.0f : x;
+    z = (z + FLT_EPSILON <= 0) ? -1.0f : z;
+    y = (y + FLT_EPSILON <= 0) ? -1.0f : y;
+
+    return glm::ivec3((int)x, (int)y, (int)z);
 }
 
 unsigned int __cubeMorton(int x, int y, int z) { // TODO 이러면 된다는데... 비트연산자, 저거 16진수도 뭔지 잘 모르겠고. https://stackoverflow.com/questions/1024754/how-to-compute-a-3d-morton-number-interleave-the-bits-of-3-ints
-
 
     x = (x >= 0) ? x : -1;
     y = (y >= 0) ? y : -1;
@@ -350,7 +460,6 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
-
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         cam.processKeyboard(FORWARD);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
@@ -361,10 +470,6 @@ void processInput(GLFWwindow* window)
         cam.processKeyboard(RIGHT);
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 
